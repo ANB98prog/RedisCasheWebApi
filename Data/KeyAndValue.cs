43150aa5df352177ce44dc0d823d0ff473cc5eb1
Alex Newman <alex.newman.prog@gmail.com>
2023-02-21 23:07:44 +0600

    Added db models and context

--- Data/KeyAndValue.cs
+++ Data/KeyAndValue.cs
@@ -2,7 +2,7 @@ namespace RedisCasheWebApi.Data;
 
 public class KeyAndValue
 {
-    public string Key {get; set;}
+    public string Key { get; set; }
 
-    public string Value {get; set;}
+    public string Value { get; set; }
 }
--- Data/KeyAndValueContext.cs
+++ Data/KeyAndValueContext.cs
@@ -1,5 +1,50 @@
 using Microsoft.EntityFrameworkCore;
+
+namespace RedisCasheWebApi.Data;
+
 public class KeyAndValueContext : DbContext
 {
+    public KeyAndValueContext(DbContextOptions<KeyAndValueContext> options) : base(options)
+    {
+    }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<KeyAndValue>()
+            .HasKey(e => e.Key);
+    }
+
+    public DbSet<KeyAndValue>? KeyAndValues { get; set; }
+}
+
+public static class Seeder
+{
+    public static async Task SeedAsync(this KeyAndValueContext context)
+    {
+        if (!context.KeyAndValues.Any())
+        {
+            int i = 1;
+            foreach (string letter in Alphabet)
+            {
+                context.Add(new KeyAndValue
+                {
+                    Key = letter,
+                    Value = $"The letter \"{letter}\" is at position {i++} in the alphabet"
+                });
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static IEnumerable<string> Alphabet
+    {
+        get
+        {
+            for (char letter = 'a'; letter <= 'z'; letter++)
+            {
+                yield return letter.ToString();
+            }
+        }
+    }
 }
